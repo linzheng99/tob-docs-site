3c2d5d342695c8e93b98112d0efd00ae2d6ece94
linzheng <alidlg9045@126.com>
2022-10-17 14:31:21 +0800

    vitepress-template: Home page configuration

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+  title: 'VitePress-template',
+  themeConfig: {
+    logo: '/logo.png',
+    siteTitle: 'VitePress-template'
+  }
+})
