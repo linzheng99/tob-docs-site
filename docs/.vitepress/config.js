902b4a009e8c8e4abddf1eddbff08ea3e7d74650
linzheng <alidlg9045@126.com>
2022-10-17 18:27:19 +0800

    docs sidebar add example

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -9,12 +9,28 @@ export default defineConfig({
     socialLinks: [
       { icon: 'github', link: 'https://github.com/linzhengGithub/vitepress-template' }
     ],
+    // 需申请
+    // algolia: {
+    //   apiKey: '<API_KEY>',
+    //   indexName: '<INDEX_NAME>',
+    //   // If Algolia did not provided you any `appId`, use `BH4D9OD16A` or remove this option
+    //   appId: '<APP_ID>',
+    // },
     sidebar: [
       {
         text: 'Introduction',
+        collapsible: true,
         items: [
-          { text: 'What is VitePress?', link: '/guide/introduction/index' },
-          { text: 'Getting Started', link: '/guide/gettingStarted/index' },
+          { text: 'What is VitePress?', link: '/guide/introduction/' },
+          { text: 'Getting Started', link: '/guide/gettingStarted/' },
+        ]
+      },
+      {
+        text: 'Component',
+        collapsible: true,
+        items: [
+          { text: 'Child-one', link: '/guide/components/Child-one/' },
+          { text: 'Child-two', link: '/guide/components/Child-two/' },
         ]
       }
     ]
