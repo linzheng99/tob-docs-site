b79128e16c69a81b7eaa30a862188e5331e0b465
linzheng <alidlg9045@126.com>
2022-10-18 09:29:13 +0800

    按照源码重构config.js代码

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -5,6 +5,7 @@ export default defineConfig({
   themeConfig: {
     logo: '/logo.png',
     siteTitle: 'VitePress-template',
+    nav: nav(),
     // navbar link
     socialLinks: [
       { icon: 'github', link: 'https://github.com/linzhengGithub/vitepress-template' }
@@ -16,23 +17,35 @@ export default defineConfig({
     //   // If Algolia did not provided you any `appId`, use `BH4D9OD16A` or remove this option
     //   appId: '<APP_ID>',
     // },
-    sidebar: [
-      {
-        text: 'Introduction',
-        collapsible: true,
-        items: [
-          { text: 'What is VitePress?', link: '/guide/introduction/' },
-          { text: 'Getting Started', link: '/guide/gettingStarted/' },
-        ]
-      },
-      {
-        text: 'Component',
-        collapsible: true,
-        items: [
-          { text: 'Child-one', link: '/guide/components/Child-one/' },
-          { text: 'Child-two', link: '/guide/components/Child-two/' },
-        ]
-      }
-    ]
+    sidebar: {
+      '/guide/': sidebarGuide()
+    }
   }
 })
+
+function nav() {
+  return [
+    { text: 'Guide', link: '/guide/introduction/whatisthis/', activeMatch: '/guide/' },
+  ]
+}
+
+function sidebarGuide() {
+  return [
+    {
+      text: 'Introduction',
+      collapsible: true,
+      items: [
+        { text: 'What is VitePress?', link: '/guide/introduction/whatisthis/' },
+        { text: 'Getting Started', link: '/guide/introduction/gettingStarted/' },
+      ]
+    },
+    {
+      text: 'Component',
+      collapsible: true,
+      items: [
+        { text: 'Child-one', link: '/guide/components/Child-one/' },
+        { text: 'Child-two', link: '/guide/components/Child-two/' },
+      ]
+    }
+  ]
+}
