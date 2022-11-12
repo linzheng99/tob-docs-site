2fa1b190eb1da032867ff20fbe93eb3fd2da3415
linzheng <alidlg9045@126.com>
2022-11-12 21:56:11 +0800

    更新vite配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,22 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import Unocss from 'unocss/vite'
+import * as path from 'path'
 
 // https://vitejs.dev/config/
 
 export default defineConfig({
+  resolve: {
+    // 设置别名
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
   plugins: [
     vue(),
     Unocss({})
   ],
+  server: {
+    host: true
+  }
 });
