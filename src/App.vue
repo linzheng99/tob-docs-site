c4c692fd27a0c31ca5d7abfdb192e2d728effeea
linzheng <alidlg9045@126.com>
2022-11-12 21:57:30 +0800

    重构 App 初始化

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div id="app" >
+    <h1>component test</h1>
+    <CountBtn/>
+  </div>
+</template>
+
+<style scoped>
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
 import { createApp } from "vue";
 import 'uno.css'
+import App from './App.vue'
 
-import count from "./components/count/index.vue";
+import Components from './entry';
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
-createApp(count).mount("#app");
+async function bootstrap() {
+  const app = createApp(App)
+
+  app.use(Components)
+  app.use(ElementPlus)
+
+  app.mount('#app')
+}
+
+bootstrap()
