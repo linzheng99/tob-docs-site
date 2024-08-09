00040ffd241d01d2be5c8d529764b2995cdfe6d0
linzheng <alidlg9045@126.com>
2024-08-09 11:42:57 +0800

    docs build

---
+++ docs/.vitepress/dist/assets/sidebar_Introduction_structure.md.0bd629bc.js
@@ -0,0 +1,49 @@
+import{_ as s,o as a,c as n,d as e}from"./app.ee4d2973.js";const _=JSON.parse('{"title":"目录说明","description":"","frontmatter":{},"headers":[],"relativePath":"sidebar/Introduction/structure.md","lastUpdated":1723173064000}'),p={name:"sidebar/Introduction/structure.md"},l=e(`<h1 id="目录说明" tabindex="-1">目录说明 <a class="header-anchor" href="#目录说明" aria-hidden="true">#</a></h1><div class="language-"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki material-theme-palenight" tabindex="0"><code><span class="line"><span style="color:#babed8;">am-template</span></span>
+<span class="line"><span style="color:#babed8;">├─ build                              // vite 构建相关配置和插件</span></span>
+<span class="line"><span style="color:#babed8;">│  ├─ config                          // 构建打包配置</span></span>
+<span class="line"><span style="color:#babed8;">│  ├─ plugin                          // 构建插件</span></span>
+<span class="line"><span style="color:#babed8;">│  └─ utils                           // 构建相关工具函数</span></span>
+<span class="line"><span style="color:#babed8;">├─ docs                               // 项目文档</span></span>
+<span class="line"><span style="color:#babed8;">├─ mock                               // mock 数据</span></span>
+<span class="line"><span style="color:#babed8;">├─ public                             // 公共目录(文件夹里面的资源打包后会在根目录下)</span></span>
+<span class="line"><span style="color:#babed8;">├─ src</span></span>
+<span class="line"><span style="color:#babed8;">│  ├─ api                             // 接口api管理</span></span>
+<span class="line"><span style="color:#babed8;">│  ├─ assets                          // 静态资源管理</span></span>
+<span class="line"><span style="color:#babed8;">│     ├─ images                       // 存放png等图片</span></span>
+<span class="line"><span style="color:#babed8;">│     └─ svg-icon                     // 存放svg图片</span></span>
+<span class="line"><span style="color:#babed8;">│  ├─ components                      // 项目组件管理</span></span>
+<span class="line"><span style="color:#babed8;">│  ├─ enums                           // 项目枚举值管理</span></span>
+<span class="line"><span style="color:#babed8;">│  ├─ hooks                           // 组合式的函数hooks</span></span>
+<span class="line"><span style="color:#babed8;">│  ├─ layouts                         // 布局组件管理</span></span>
+<span class="line"><span style="color:#babed8;">│     ├─ default                      // 默认布局组件</span></span>
+<span class="line"><span style="color:#babed8;">│       ├─ Header                     // 头部</span></span>
+<span class="line"><span style="color:#babed8;">│       ├─ Tabs                       // 标签页</span></span>
+<span class="line"><span style="color:#babed8;">│       ├─ Sidebar                    // 侧边栏</span></span>
+<span class="line"><span style="color:#babed8;">│       └─ Content                    // 主体内容</span></span>
+<span class="line"><span style="color:#babed8;">│     └─ common                       // 布局公共组件管理</span></span>
+<span class="line"><span style="color:#babed8;">│  ├─ router                          // vue路由</span></span>
+<span class="line"><span style="color:#babed8;">│     ├─ guard                        // 路由守卫</span></span>
+<span class="line"><span style="color:#babed8;">│     └─ routes                       // 声明的固定路由</span></span>
+<span class="line"><span style="color:#babed8;">│  ├─ store                           // pinia 状态管理</span></span>
+<span class="line"><span style="color:#babed8;">│  ├─ styles                          // 全局样式</span></span>
+<span class="line"><span style="color:#babed8;">│  ├─ theme                           // 主题配置</span></span>
+<span class="line"><span style="color:#babed8;">│  ├─ typings                         // TS类型声明文件(*.d.ts)</span></span>
+<span class="line"><span style="color:#babed8;">│  ├─ utils                           // 全局工具函数</span></span>
+<span class="line"><span style="color:#babed8;">│     ├─ axios                        // axios</span></span>
+<span class="line"><span style="color:#babed8;">│       ├─ initAxios.ts               // 封装axios</span></span>
+<span class="line"><span style="color:#babed8;">│       └─ index.ts                   // 创建axios</span></span>
+<span class="line"><span style="color:#babed8;">│     ├─ helper                       // 辅助函数</span></span>
+<span class="line"><span style="color:#babed8;">│     └─ lib                          // 库</span></span>
+<span class="line"><span style="color:#babed8;">│       └─ echarts.ts                 // echarts相关</span></span>
+<span class="line"><span style="color:#babed8;">│  ├─ views                           // 页面</span></span>
+<span class="line"><span style="color:#babed8;">│  ├─ App.vue                         // vue文件入口</span></span>
+<span class="line"><span style="color:#babed8;">│  └─ main.ts                         // 项目入口ts文件</span></span>
+<span class="line"><span style="color:#babed8;">├─ .env                               // 环境文件</span></span>
+<span class="line"><span style="color:#babed8;">├─ .env-config.ts                     // 请求环境的配置文件</span></span>
+<span class="line"><span style="color:#babed8;">├─ .env.development                   // 开发环境文件</span></span>
+<span class="line"><span style="color:#babed8;">├─ .env.production                    // 生产环境文件</span></span>
+<span class="line"><span style="color:#babed8;">├─ package.json                       // npm依赖描述文件</span></span>
+<span class="line"><span style="color:#babed8;">├─ index.html</span></span>
+<span class="line"><span style="color:#babed8;">├─ README.md</span></span>
+<span class="line"><span style="color:#babed8;">└─ vite.config.ts                     // vite配置</span></span>
+<span class="line"><span style="color:#babed8;"></span></span></code></pre></div>`,2),o=[l];function c(t,b,i,r,d,y){return a(),n("div",null,o)}const m=s(p,[["render",c]]);export{_ as __pageData,m as default};
